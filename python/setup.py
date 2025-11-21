c8a7d535ba0ff6f7b5b3b2ce0a5af9ed9c3abc0d
Alvian Rahman Hanif <alvian.hanif@pasarpolis.com>
2025-11-21 22:25:18 +0700

    setup for PyPI

--- python/setup.py
+++ python/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="unilog",
+    name="commonlog",
     version="0.1.0",
     description="Unified logging and alerting library for Python.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Alvian Rahman Hanif",
     author_email="alvian.hanif@pasarpolis.com",
-    url="https://gitlab.com/pasarpolis/unilog",
+    url="https://github.com/alvianhanif/commonlog",
     packages=find_packages(),
     install_requires=[],
     python_requires=">=3.6",
